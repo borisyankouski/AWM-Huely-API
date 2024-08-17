74a9804b97c18d0857b3e4a2801ec549c70dde20
Boris-Yankouski <boris.yankouski@student.odisee.be>
2024-08-17 23:22:47 +0200

    Kleine fix voor extra details bij failed logins

--- login.php
+++ login.php
@@ -15,27 +15,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     // Check if username and password are provided
     if(empty($username) || empty($password)){
-        echo '{"error":"Missing username or password","status":"fail"}';
+        echo '{"error":"Missing username or password","status":"fail-missing"}';
         exit;
     }
 
     // Fetch user data based on username
     $stmt = $conn->prepare("SELECT * FROM user WHERE username = ?");
     if (!$stmt) {
-        echo '{"error":"Prepared Statement failed for user lookup","status":"fail"}';
+        echo '{"error":"Prepared Statement failed for user lookup","status":"fail-prepare"}';
         exit;
     }
 
     $stmt->bind_param("s", $username);
     if (!$stmt->execute()) {
-        echo '{"error":"Failed to execute user lookup","status":"fail"}';
+        echo '{"error":"Failed to execute user lookup","status":"fail-execute"}';
         $stmt->close();
         exit;
     }
 
     $result = $stmt->get_result();
     if (!$result || $result->num_rows === 0) {
-        echo '{"error":"Credentials mismatch","status":"fail"}';
+        echo '{"error":"Credentials mismatch","status":"fail-credentials"}';
         $stmt->close();
         exit;
     }
@@ -44,6 +44,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     $hashedPassword = $userRow['password'];
     $userRow['password'] = "hidden"; 
 
+    // Check if user is verified
+    if ($userRow['isVerified'] == 0) {
+        echo '{"error":"User is unverified","status":"fail-verification"}';
+        $stmt->close();
+        exit;
+    }
+
     $stmt->close();
 
     // Verify password
@@ -51,9 +58,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         // echo '{"data":"Successful login","status":"ok"}';
         echo '{"data":' . json_encode($userRow) . ',"status":"ok"}';
     } else {
-        echo '{"error":"Credentials mismatch","status":"fail"}';
+        echo '{"error":"Credentials mismatch","status":"fail-credentials"}';
     }
 } else {
-    echo '{"error":"Invalid request method","status":"fail"}';
+    echo '{"error":"Invalid request method","status":"fail-invalid"}';
 }
 ?>
